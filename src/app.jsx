e309c32ea3f695f5ea65d39f887c55530218e812
chenglin <837460387@qq.com>
2019-06-12 11:44:26 +0800

    'initial'

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
+import './index.scss'
+
+ReactDOM.render(
+  <div>
+    here
+    <i className="fa fa-bath"></i>
+    <h1>Hello, world!</h1>,
+  </div>,
+  document.getElementById('app')
+)
